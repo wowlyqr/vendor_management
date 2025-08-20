1e7a8f80b09e24413b2cfe4e0fa1f3ba3aa87853
Mahalakshmi <mahalakshmi@dreaminfinity.in>
2025-08-20 19:21:07 +0530

    allow

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from app.extensions import socketio
 app = create_app()
 
 if __name__ == "__main__":
-    socketio.run(app,host='0.0.0.0', debug=True) 
+    socketio.run(app,host='0.0.0.0', debug=True,allow_unsafe_werkzeug=True) 
